d61fd4b282d58254a48cccaf592bd2ecf2288c0f
Ted Wu <tedwu2016@u.northwestern.edu>
2017-08-10 23:29:53 -0700

    Initial requests

--- server.js
+++ server.js
@@ -10,17 +10,71 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/topstories', function(req, res) {
-  const api = 'https://hacker-news.firebaseio.com/v0/topstories.json';
-  const numStories = 30;
+  const hacker_api = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+  const numStories = 1;
   request.get({
-    'url': api,
+    'url': hacker_api,
     'json': true,
   }, function(err, response, body){
-    const topStories = response.body.slice(0,30);
+    const topStories = response.body.slice(0,numStories);
+    async.forEach(topStories, fetchStory);
+
     res.json({ topStories });
   });
 });
 
+const fetchStory = (storyId) => {
+  async.parallel([
+    function(callback) {
+      request.get({
+        'url': 'https://hacker-news.firebaseio.com/v0/item/' + storyId + '.json?',
+        'json': true,
+      }, function(err, response, body){
+        if(err) return callback(err);
+        const comments = response.body.kids || 0;
+        if(comments.length > 0) {
+          console.log("first comments", comments);
+          async.forEach(comments, fetchComments);
+        }
+
+        callback();
+      });
+    },
+  ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+    if (err) return next(err); //If an error occurred, we let express handle it by calling the `next` function
+      //when both are done
+
+  });
+}
+
+// const fetchAndIncrementCount = (comment) => {
+//   console.log(comment);
+
+// }
+
+const fetchComments = (commentId) => {
+  async.parallel([
+    function(callback) {
+      request.get({
+        'url': 'https://hacker-news.firebaseio.com/v0/item/' + commentId + '.json?',
+        'json': true,
+      }, function(err, response, body){
+        if(err) return callback(err);
+        const comments = response.body.kids || 0;
+        if(comments.length > 0) {
+          // async.forEach(comments, fetchAndIncrementCount);
+          console.log("comments", comments);
+        }
+
+        callback();
+      });
+    },
+  ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+    if (err) return next(err); //If an error occurred, we let express handle it by calling the `next` function
+      //when both are done
+
+  });
+}
 
 var port = process.env.PORT || 8080;
 app.listen(port);
