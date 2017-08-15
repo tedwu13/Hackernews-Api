060467337e64cec663c7de7c86488fe20dd2dd08
Ted Wu <ted.wu@chartboost.com>
2017-08-14 23:06:14 -0700

    can call the same fetch function

--- server.js
+++ server.js
@@ -11,73 +11,58 @@ app.use(morgan('dev'));
 
 app.get('/topstories', function(req, res) {
   const hacker_api = 'https://hacker-news.firebaseio.com/v0/topstories.json';
-  const numStories = 1;
+  const numStories = 2;
   request.get({
     'url': hacker_api,
     'json': true,
   }, function(err, response, body){
     const topStories = response.body.slice(0,numStories);
+    console.log(topStories);
     async.map(topStories, fetchStory);
 
     res.json({ topStories });
   });
 });
 
-let finalResponse = {};
-const fetchStory = (storyId) => {
-  async.parallel([
+let finalDictionary = {};
+let userDict = {};
+
+const fetchStory = (id) => {
+  let storyName;
+  let user;
+  async.series([
     function(callback) {
+      // Fetch story by getting the initial comments
       request.get({
-        'url': 'https://hacker-news.firebaseio.com/v0/item/' + storyId + '.json?',
+        'url': 'https://hacker-news.firebaseio.com/v0/item/' + id + '.json?',
         'json': true,
       }, function(err, response, body){
         if(err) return callback(err);
         // Check if children array is empty, if it is have a fallback value as 0
         const comments = response.body.kids || 0;
-        const user = response.body.by || 'no_user';
-
+        user = response.body.by || 'No User';
+        story = response.body.title || 'No Title';
         if(comments === 0) {
-          return "No Comments Available";
+          // console.log("No comments");
         } else {
-          console.log("First comments", comments, response.body.title);
-          async.map(comments, fetchComments);
+          // console.log("First comments", comments, response.body.title);
+          if (user in userDict) {
+              userDict[user] += 1;
+          } else {
+              userDict[user] = 1;
+          }
+          return async.map(comments, fetchStory, userDict);
         }
-        // callback();
+        callback();
       });
     },
   ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
     if (err) return next(err); //If an error occurred, we let express handle it by calling the `next` function
       //when both are done
-
+      console.log(userDict);
   });
 }
 
-const fetchComments = (commentId) => {
-  request.get({
-    'url': 'https://hacker-news.firebaseio.com/v0/item/' + commentId + '.json?',
-    'json': true,
-  }, function(err, response, body){
-    if(err) return callback(err);
-    const comments = response.body.kids || 0;
-    const user = response.body.by || 'no_user';
-      if(comments === 0) {
-        if (user in finalResponse) {
-            finalResponse[user] += 1;
-        } else {
-            finalResponse[user] = 1;
-        }
-      } else {
-        console.log(commentId, comments);
-        _.map(comments, fetchComments);
-      }
-  });
-
-  // console.log("finalResponse", finalResponse);
-  var counter = 0;
-  _.forEach(finalResponse, function(value, key) {
-    counter += value;
-  })
-}
 
 var port = process.env.PORT || 8080;
 app.listen(port);
