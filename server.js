9d0a00be7ff86e7eda3cd7ea07470776b4d8155f
Ted Wu <ted.wu@chartboost.com>
2017-08-15 03:44:08 -0700

    use promises while traversing the nested arrays

--- server.js
+++ server.js
@@ -11,58 +11,107 @@ app.use(morgan('dev'));
 
 app.get('/topstories', function(req, res) {
   const hacker_api = 'https://hacker-news.firebaseio.com/v0/topstories.json';
-  const numStories = 2;
+  const numStories = 10;
   request.get({
     'url': hacker_api,
     'json': true,
   }, function(err, response, body){
     const topStories = response.body.slice(0,numStories);
-    console.log(topStories);
-    async.map(topStories, fetchStory);
+    async.map(topStories, fetchStorySetup);
+    // fetchStorySetup('15014757');
 
     res.json({ topStories });
   });
 });
 
-let finalDictionary = {};
-let userDict = {};
-
-const fetchStory = (id) => {
-  let storyName;
-  let user;
-  async.series([
-    function(callback) {
-      // Fetch story by getting the initial comments
-      request.get({
-        'url': 'https://hacker-news.firebaseio.com/v0/item/' + id + '.json?',
-        'json': true,
-      }, function(err, response, body){
-        if(err) return callback(err);
-        // Check if children array is empty, if it is have a fallback value as 0
-        const comments = response.body.kids || 0;
-        user = response.body.by || 'No User';
-        story = response.body.title || 'No Title';
-        if(comments === 0) {
-          // console.log("No comments");
-        } else {
-          // console.log("First comments", comments, response.body.title);
-          if (user in userDict) {
-              userDict[user] += 1;
-          } else {
-              userDict[user] = 1;
-          }
-          return async.map(comments, fetchStory, userDict);
+const fetchStorySetup = (id) => {
+  let dict = {};
+  let allComments = [];
+  
+  let topCommentsPromise = getTopCommentsForTopic(id);
+  Promise.resolve(topCommentsPromise).then(function(topComments) {
+    let traversePromises = [];
+    topComments.children.forEach(function(topCommentId) {
+      traversePromises.push(traverse(topCommentId, dict, allComments));
+    });
+    Promise.all(traversePromises).then(function() {
+      let sum = 0;
+      for (var key in dict) {
+        if (dict.hasOwnProperty(key)) {
+          sum += dict[key];
         }
-        callback();
-      });
-    },
-  ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
-    if (err) return next(err); //If an error occurred, we let express handle it by calling the `next` function
-      //when both are done
-      console.log(userDict);
+      }
+      console.log(dict);
+      console.log(sum);
+      // console.log(allComments);
+    })
+  });
+
+  // let traversalPromise = traverse(id, allComments);
+  // Promise.resolve(traversalPromise).then(function() {
+  //   console.log(allComments);
+  // });
+}
+
+function getTopCommentsForTopic(topic_id) {
+  return new Promise((resolve, reject) => {
+    request.get({
+    'url': 'https://hacker-news.firebaseio.com/v0/item/' + topic_id + '.json?',
+    'json': true,
+    }, function(err, response, body) {
+      if (err) reject(err);
+      return resolve({children: body.kids});
+    });
   });
 }
 
+function traverse(commentId, dict, allComments) {
+  return new Promise((resolve, reject) => {
+    let getCommentPromise = fetch_comment(commentId);
+    Promise.resolve(getCommentPromise).then(function(comment) {
+
+      // add stuff
+      let author = comment.author;
+      if (author) {
+        if (author in dict) {
+          dict[author] += 1;
+        } else {
+          dict[author] = 1;
+        }
+      }
+      allComments.push(comment);
+
+      if (comment.children.length === 0) {
+        resolve();
+      } else {
+        let traversePromises = []
+        comment.children.forEach(function(childId) {
+          traversePromises.push(traverse(childId, dict, allComments));
+        });
+        Promise.all(traversePromises).then(function() {
+          resolve();
+        })
+      }
+    });
+  });
+};
+
+function fetch_comment(comment_id, callback) {
+  // console.log("Fetching " + comment_id);
+  return new Promise((resolve, reject) => {
+    request.get({
+      'url': 'https://hacker-news.firebaseio.com/v0/item/' + comment_id + '.json?',
+      'json': true,
+    }, function(err, response, body) {
+      if (err) {
+        reject(err);
+      }
+      let author = body.by;
+      let children = body.kids === undefined ? [] : body.kids;
+      resolve({author: author, children: children})
+    });
+  })
+}
 
 var port = process.env.PORT || 8080;
 app.listen(port);
